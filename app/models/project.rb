212390983332b15ad64a24280baa0a3cdf638bbd
Sergio Diaz <sergio.diazg92@gmail.com>
2020-07-15 17:48:55 -0500

    project model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 3 }
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  
+    let(:project) { Project.new }
+
+    context 'when project has no name' do
+        it 'is not valid' do
+            expect(project).to_not be_valid
+        end
+    end
+
+    context 'when project has a name' do
+        it 'validates presence and length' do
+            project.name = 'Name'
+            expect(project).to be_valid
+        end
+
+        it 'is not valid when length is less than 3' do
+            project.name = 'he'
+            expect(project).to_not be_valid
+        end
+    end
 end
